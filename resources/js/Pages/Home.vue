91bea7595bc8d91266e506254d44522dd38b0a6c
Freeku132 <freeku132@gmail.com>
2022-10-07 12:30:00 +0200

    Start

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="bg-gray-100 flex justify-between p-5 ">
+        <div>
+            <h1 class="h1 light:text-red-600 dark:text-blue-600">Page Title</h1>
+
+        </div>
+        <div class="flex space-x-8">
+            <a href="#" class="link">
+                <p>
+                    Main Page
+                </p>
+            </a>
+            <a href="#" class="link">
+                <p>
+                    About Project
+                </p>
+            </a>
+        </div>
+
+    </nav>
+    <h1>
+        Hello world
+    </h1>
+
+</template>
+
+<script>
+
+    localStorage.theme = 'dark';
+
+</script>
+
+<script setup>
+</script>
