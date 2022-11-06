bfc5f6a1f3339a6d9e8ffbafafbc870bfa9705bb
Freeku132 <freeku132@gmail.com>
2022-11-06 23:31:19 +0100

    Add Vue Toastification

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,25 @@ import { createInertiaApp, Head } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+const options = {
+    position: "top-right",
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+}
 
 createInertiaApp({
     title: (title) => `${appName} - ${title}`,
@@ -16,6 +33,7 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(Toast, options)
             .component("Head", Head)
             .mount(el);
     },
